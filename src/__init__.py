3e4080abc22063e6a31549bbdf913ed583cec19e
vosmann <vosmann@gmail.com>
2014-07-30 00:44:21 +0200

    Started large refactoring.

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, make_response
-from pymongo import MongoClient
-from project import Project
-
-shared_mongo_client = MongoClient()
-app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run(debug=True)
