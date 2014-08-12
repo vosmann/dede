4f9a715e2e9d7c9b09599d1c3aef22c28bf8d311
vosmann <vosmann@gmail.com>
2014-08-13 00:56:30 +0200

    Starting the front-end of the edit app.

--- src/flask/static/js/edit-app/editControllers.js
+++ src/flask/static/js/edit-app/editControllers.js
@@ -1,27 +1,31 @@
-var dedeEditControllers = angular.module("dedeEditControllers", []);
+
+var dedeEditControllers = angular.module("dedeEditControllers", ['ui.bootstrap']);
+
+dedeEditControllers.controller("PageOrEntryCtrl", function($scope) {
+            $scope.pageOrEntry = 'page';
+        });
 
 dedeEditControllers.controller("PageNamesDropdownCtrl", ["$scope", "PageNames",
         function($scope, PageNames) {
             $scope.pageNames = PageNames.query();
-
-            //$scope.status = {
-                //isopen: false
-            //};
-
-            //$scope.toggled = function(open) {
-                //console.log('PageDropdown is now: ', open);
-            //};
-
-            //$scope.toggleDropdown = function($event) {
-                //$event.preventDefault();
-                //$event.stopPropagation();
-                //$scope.status.isopen = !$scope.status.isopen;
-            //};
+            $scope.selectedPageName = $scope.pageNames[0];
+            $scope.set = function(pageName) {
+                $scope.selectedPageName = pageName;
+            };
+            $scope.status = {
+                isopen: false
+            };
+            $scope.toggleDropdown = function($event) {
+                $event.preventDefault(); // defaultPrevented() instead?
+                $event.stopPropagation();
+                $scope.status.isopen = !$scope.status.isopen;
+            };
 
         }]);
 
 dedeEditControllers.controller("TagsCtrl", ["$scope", "Tags",
         function($scope, Tags) {
+            var newTag = ['Add new tag'];
             $scope.tags = Tags.query();
         }]);
 
