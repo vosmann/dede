d60187912ab1f1414da7c32900ecb66a19924e53
vosmann <vosmann@gmail.com>
2014-12-07 14:17:39 +0100

    ViewPage now contains a map of entries instead of a list.

--- src/flask/entities/view_page.py
+++ src/flask/entities/view_page.py
@@ -1,9 +1,9 @@
 class ViewPage:
-    """A view page is denormalized, i.e. contains the page's entries in their entirity.
+    """A view page is denormalized, i.e. contains a map of all the page's complete entries.
     It is what is shown to the client. Current implementation does not include paging."""
 
     def __init__(self, page): 
         self._id = page._id
         self.name = page.name
-        self.entries = []
+        self.entries = {}
 
--- src/flask/serve-view.py
+++ src/flask/serve-view.py
@@ -17,7 +17,7 @@ from entities.view_entry import ViewEntry
 #   Cache:
 #       * page ids
 #       * page names
-#       * all pages
+#       * all page views
 # - The cache could never expire (or expire like every 2-3 hours, but it should be invalidated when edits are made.
 
 
@@ -54,11 +54,11 @@ def get_page(page_id):
 # A "view page" is a list of completely assembled pages ready to be shown.
 # Must move into a separate module, obviously.
 def assemble_view_page(page):
-    view_entries = []
+    view_entries = {}
     for entry_id in page.entry_ids:
         db_entry = mongo.dede.entries.find_one(id_query(entry_id))
         view_entry = ViewEntry(Entry(db_entry)) 
-        view_entries.append(view_entry.json_dict())
+        view_entries[view_entry._id] = view_entry.json_dict()
 
     view_page = ViewPage(page)
     view_page.entries = view_entries
