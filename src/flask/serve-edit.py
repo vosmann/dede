ab658fd9723e0d7da9f7cdae7d3e525a54c38215
vosmann <vosmann@gmail.com>
2015-01-02 21:58:31 +0100

    Both development servers now listen for conns from anywhere.

--- src/flask/serve-edit.py
+++ src/flask/serve-edit.py
@@ -366,5 +366,5 @@ def name_query(name):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
--- src/flask/serve-view.py
+++ src/flask/serve-view.py
@@ -111,5 +111,5 @@ def id_query(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4000)
+    app.run(debug=True, port=4000, host='0.0.0.0')
 
