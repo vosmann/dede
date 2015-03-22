504e46f29398d32f2ace779f93860fc90d638f79
vosmann <vosmann@gmail.com>
2015-03-23 00:13:39 +0100

    Started on build.sh.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,16 @@
-
-# kopiraj cijelu strukturu projekta pa *onda* mijenjaj svaki file njegovom obrađenom verzijom
-# ILI
-# obrađuj svaki file pojedinačno i postupno napravi strukturu.
+mkdir -p ../target/dede/
+mkdir -p ../target/temp/python-minified
 
 find ../src -iname *py
+
 pyminifier --obfuscate dede_view_app.py | grep -i "#" > file
-find *.txt -exec awk 'END {print $0 "," FILENAME}' {} \;
+# find *.txt -exec awk 'END {print $0 "," FILENAME}' {} \;
 
-mkdir -p ../target/dede/
+
+cp --parents he/lo/you/mmm dest
+DIR_NAME=dirname $FULL_PATH
+FILE_NAME=basename $FULL_PATH
+
+
 tar -cvzf ../target/dede.tar.gz ../dede/
-
+rm -r ../target/temp
