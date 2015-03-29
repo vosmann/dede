0cca7d3050da87e9341d0b21033f9df521534a94
vosmann <vosmann@gmail.com>
2015-03-29 22:05:05 +0200

    Build.sh in progress.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,52 +1,72 @@
 #!/bin/bash
+# Build an archive with Bash? Well, why not
+# Attempting readability by extracting params passed into functions into local
+# variables maybe isn't that readable. Perhaps better to just have global vars.
 
 function find_by_extension {
-    local EXTENSION=$1
-    local FULL_PATHS=$(find ../.. -iname *.$EXTENSION)
+    local ROOT_DIR=$1
+    local EXTENSION=$2
+    local FULL_PATHS=$(find $ROOT_DIR -iname *.$EXTENSION)
     echo "$FULL_PATHS"
 }
-# Python: minify, for kicks
 function process_one_py {
-    PY_FULL_PATH=$1
-    PY_PATH=$(dirname $PY_FULL_PATH)
-    PY_NAME=$(basename $PY_FULL_PATH)
-    echo "Here's one path in a function: $PY_PATH"
-    echo "and one name in a function: $PY_NAME"
+    local PY_FULL_PATH=$1
+    local ROOT_DIR=$2/
+    local TARGET_DIR=$3
+    local TEMP_DIR=$4
+    local PY_PATH=$(dirname $PY_FULL_PATH)
+    local PY_NAME=$(basename $PY_FULL_PATH)
+    local PY_RELATIVE_PATH=${PY_PATH#$ROOT_DIR} # Chop off prefix, i.e. root dir 
+    # pyminifier --obfuscate PY_FULL_PATH | grep -i "#" > $TEMP_PY_DIR/$PY_NAME # Minify, for kicks
+    echo "PY_FULL_PATH=$PY_FULL_PATH"
+    echo "ROOT_DIR=$ROOT_DIR"
+    echo "TARGET_DIR=$TARGET_DIR"
+    echo "TEMP_DIR=$TEMP_DIR"
+    echo "PY_PATH=$PY_PATH"
+    echo "PY_NAME=$PY_NAME"
+    echo "PY_RELATIVE_PATH=$PY_RELATIVE_PATH"
     echo ""
-    # pyminifier --obfuscate dede_view_app.py | grep -i "#" > file
-    # find *.txt -exec awk 'END {print $0 "," FILENAME}' {} \;
-    #cp --parents he/lo/you/mmm dest
+    # cp --parents PY_FULL_PATH ../target
+    mkdir -pv $TARGET/$PY_PATH
 }
 function process_py {
-    PY_FILES=$(find_by_extension py)
-    echo "PY_FILES are =$PY_FILES"
-    TEMP_LOCATION=../target/temp/python-minified
-    mkdir -p $TEMP_LOCATION
+    local ROOT_DIR=$1
+    local TARGET_DIR=$2
+    local TEMP_DIR=$TARGET_DIR/py_temp
+    mkdir -pv $TEMP_DIR
+    # local ROOT_SRC_DIR=$ROOT_DIR/src
+    PY_FILES=$(find_by_extension $ROOT_DIR py)
+    # echo "PY_FILES are =$PY_FILES"
     for PY_FILE in $PY_FILES; do
-        process_one_py $PY_FILE
+        process_one_py $PY_FILE $ROOT_DIR $TARGET_DIR $TEMP_DIR
     done
 }
-# HTML
 function process_html {
+    echo "Processing HTML."
 }
-
-# CSS and similar
 function process_css {
+    echo "Processing CSS"
+}
+function process_js {
+    echo "Processing JS"
 }
 
-# JS: not using a fully-blown JS build process
+echo "Started dede.tar.gz build."
+cd ..
+ROOT_DIR=$(pwd)
+cd scripts
+TARGET_DIR=$ROOT_DIR/target
+echo "ROOT_DIR=$ROOT_DIR"
+echo "TARGET_DIR=$TARGET_DIR"
+rm -rv $TARGET_DIR
+mkdir -pv $TARGET_DIR
 
-
-
-# Build .tar.gz
-rm -r ../target
-mkdir -p ../target/dede/
-
-$(process_py)
-#$(process_html)
-#$(process_css)
-#$(process_js)
-
+process_py $ROOT_DIR $TARGET_DIR
+#process_html
+#process_css
+#process_js
 # Compression
 #tar -cvzf ../target/dede.tar.gz ../dede/
-#rm -r ../target/temp
+#rm -rv ../target/temp
+
+
