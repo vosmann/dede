9d5090c4f932c87611050c09905801ed5152b4b1
vosmann <vosmann@gmail.com>
2015-03-29 22:22:43 +0200

    Build.sh in progress. Python files get minified and put into target/.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Build an archive with Bash? Well, why not
+# Minifying Python too; for kicks.
 # Attempting readability by extracting params passed into functions into local
 # variables maybe isn't that readable. Perhaps better to just have global vars.
 
@@ -17,32 +18,37 @@ function process_one_py {
     local PY_PATH=$(dirname $PY_FULL_PATH)
     local PY_NAME=$(basename $PY_FULL_PATH)
     local PY_RELATIVE_PATH=${PY_PATH#$ROOT_DIR} # Chop off prefix, i.e. root dir.
-    pyminifier --obfuscate PY_FULL_PATH | grep -i "#" > $TEMP_DIR/$PY_NAME # Minify, for kicks
     echo "PY_FULL_PATH=$PY_FULL_PATH"
-    echo "ROOT_DIR=$ROOT_DIR"
-    echo "TARGET_DIR=$TARGET_DIR"
-    echo "TEMP_DIR=$TEMP_DIR"
-    echo "PY_PATH=$PY_PATH"
-    echo "PY_NAME=$PY_NAME"
-    echo "PY_RELATIVE_PATH=$PY_RELATIVE_PATH"
-    echo ""
-    # cp --parents PY_FULL_PATH ../target
+    pyminifier --obfuscate $PY_FULL_PATH | grep -v "#" > $TEMP_DIR/$PY_NAME
+    # --parents? No thanks, got PY_RELATIVE_PATH.
+    mkdir -pv $TARGET_DIR/dede/$PY_RELATIVE_PATH
+    cp $TEMP_DIR/$PY_NAME $TARGET_DIR/dede/$PY_RELATIVE_PATH
+    # echo "ROOT_DIR=$ROOT_DIR"
+    # echo "TARGET_DIR=$TARGET_DIR"
+    # echo "TEMP_DIR=$TEMP_DIR"
+    # echo "PY_PATH=$PY_PATH"
+    # echo "PY_NAME=$PY_NAME"
+    # echo "PY_RELATIVE_PATH=$PY_RELATIVE_PATH"
     mkdir -pv $TARGET/$PY_PATH
 }
 function process_py {
+    echo "Processing Python."
     local ROOT_DIR=$1
     local TARGET_DIR=$2
     local TEMP_DIR=$TARGET_DIR/py_temp
     mkdir -pv $TEMP_DIR
-    # local ROOT_SRC_DIR=$ROOT_DIR/src
     PY_FILES=$(find_by_extension $ROOT_DIR py)
-    # echo "PY_FILES are =$PY_FILES"
     for PY_FILE in $PY_FILES; do
         process_one_py $PY_FILE $ROOT_DIR $TARGET_DIR $TEMP_DIR
     done
+    rm -rv $TEMP_DIR
 }
 function process_html {
     echo "Processing HTML."
+    local HTML_FILES=$(find_by_extension $ROOT_DIR html)
+    for HTML_FILE in $HTML_FILES; do
+        process_one_py $PY_FILE $ROOT_DIR $TARGET_DIR $TEMP_DIR
+    done
 }
 function process_css {
     echo "Processing CSS"
@@ -60,7 +66,6 @@ echo "ROOT_DIR=$ROOT_DIR"
 echo "TARGET_DIR=$TARGET_DIR"
 rm -rv $TARGET_DIR
 mkdir -pv $TARGET_DIR
-
 process_py $ROOT_DIR $TARGET_DIR
 #process_html
 #process_css
